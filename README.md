7dcc603dd69bf5bb6a47c8efbee3d175fd9d3b41
Owen Luke <hi.owen.luke@gmail.com>
2019-11-23 15:07:47 +0800

    init

---
+++ README.md
@@ -0,0 +1,28 @@
+
+# Benchmark-Easy
+
+An easy tools for benchmark testing of [Node.js](https://nodejs.org).
+
+## Usage
+
+```js
+const be = require('benchmark-easy')();
+const times = 1000000;
+
+const main = async () => {
+	// do something
+};
+
+be.start(main, times);
+```
+
+The result will be like below:
+```
+Done. 1000000 times, 0.221 seconds, 4524886/s
+```
+
+## License
+
+[MIT](LICENSE)
+
+Copyright (c) 2019, Owen Luke
---
+++ src/index.js
@@ -0,0 +1,39 @@
+
+const me = {
+	startTime: null,
+	handler: null,
+	count: 0,
+	maxTimes: 0,
+
+	async start(func, maxTimes) {
+		this.count = 0;
+		this.maxTimes = maxTimes;
+		this.startTime = new Date().getTime();
+
+		this.handler = setInterval(() => {this.verify()}, 1);
+
+		while(++ this.count < maxTimes) {
+			await func(this.count);
+		}
+	},
+
+	verify() {
+		const count = this.count;
+		if (count < this.maxTimes) {
+			return;
+		}
+
+		clearInterval(this.handler);
+
+		const endTime = new Date().getTime();
+		const during = (endTime - this.startTime) / 1000;
+		const avg = Math.floor(count / during);
+		console.log(`Done. ${count} times, ${during} seconds, ${avg}/s`);
+	}
+};
+
+const create = () => {
+	return Object.create(me);
+};
+
+module.exports = create;
