e126f2c1fc970bd76333d60fec0d99ecf3dfd645
Owen Luke <hi.owen.luke@gmail.com>
2019-11-25 17:46:57 +0800

    .

--- src/index.js
+++ src/index.js
@@ -1,36 +1,21 @@
 
 const me = {
-	startTime: null,
-	handler: null,
-	count: 0,
-	maxTimes: 0,
-
 	async start(func, maxTimes) {
-		this.count = 0;
-		this.maxTimes = maxTimes;
-		this.startTime = new Date().getTime();
+		const startTime = new Date().getTime();
 
-		this.handler = setInterval(() => {this.verify()}, 1);
-
-		while(++ this.count <= maxTimes) {
-			await func(this.count);
+		let count = 0;
+		while(++ count <= maxTimes) {
+			await func(count);
 		}
+
+		this.done(maxTimes, startTime);
 	},
 
-	verify() {
-		let count = this.count;
-		if (count < this.maxTimes) {
-			return;
-		}
-
-		clearInterval(this.handler);
-
-		count --;
-
+	done(maxTimes, startTime) {
 		const endTime = new Date().getTime();
-		const during = (endTime - this.startTime) / 1000;
-		const avg = Math.floor(count / during);
-		console.log(`Done. ${count} times, ${during} seconds, ${avg}/s`);
+		const during = (endTime - startTime) / 1000;
+		const avg = Math.floor(maxTimes / during);
+		console.log(`Done. ${maxTimes} times, ${during} seconds, ${avg}/s`);
 	}
 };
 
