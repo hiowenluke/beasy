2dc8f213cc2d71db9184ac55e83763dc3d785be7
Owen Luke <hi.owen.luke@gmail.com>
2019-12-06 15:18:57 +0800

    .

--- src/index.js
+++ src/index.js
@@ -1,35 +1,64 @@
 
 const me = {
-	async start(func, maxTimes) {
-		console.log(`Benchmark [${maxTimes}] times starting...`);
+	async start(func, maxTimes, numberOfProcesses = 10, runTimes = 100) {
+		console.log(`Benchmark [${maxTimes}] times [${numberOfProcesses}] processes.\nStarting...`);
 
+		const duringArr = [];
+		const rateArr = [];
+
+		for (let i = 0; i < runTimes; i ++) {
+			const result = await this.runOnce(func, maxTimes, numberOfProcesses);
+			const {during, rate} = result;
+			console.log(`Run #${i + 1}: ${during} seconds, ${rate} times/sec.`);
+
+			duringArr.push(during);
+			rateArr.push(rate);
+		}
+
+		const avgDuring = (duringArr.reduce((acc, val) => acc + val) / duringArr.length).toFixed(2);
+		const avgRate = (rateArr.reduce((acc, val) => acc + val) / rateArr.length).toFixed(0);
+
+		console.log(`Done.\nAverage: ${avgDuring} seconds, ${avgRate} times/sec.`);
+		process.exit();
+	},
+
+	async runOnce(func, maxTimes, numberOfProcesses) {
 		let startTime;
+		const averageTimes = Math.floor(maxTimes / numberOfProcesses);
 
 		const run = async () => {
-			return new Promise(async resolve => {
-				setTimeout(async () => {
-					startTime = new Date().getTime();
+			const promises = [];
 
-					let count = 0;
-					while(++ count <= maxTimes) {
-						await func(count);
-					}
+			for (let i = 0; i < numberOfProcesses; i ++) {
+				const p = new Promise(async resolve => {
+					setTimeout(async () => {
+						startTime = new Date().getTime();
 
+						let count = 0;
+						while(++ count <= averageTimes) {
+							await func(count);
+						}
+
+						resolve();
+					}, 10);
+				});
+				promises.push(p);
+			}
+
+			return new Promise(resolve => {
+				Promise.all(promises).then(values => {
 					resolve();
-				}, 10);
-			})
+				});
+			});
 		};
 
 		await run();
-		this.done(maxTimes, startTime);
-	},
 
-	done(maxTimes, startTime) {
 		const endTime = new Date().getTime();
 		const during = (endTime - startTime) / 1000;
-		const avg = Math.floor(maxTimes / during);
-		console.log(`Done. ${during} seconds, ${avg} times/sec.`);
-		process.exit();
+		const rate = Math.floor(maxTimes / during);
+
+		return {during, rate};
 	}
 };
 
---
+++ test/multi-processes.js
@@ -0,0 +1,9 @@
+
+const be = require('../src')();
+const times = 1000000;
+
+const main = async () => {
+	// do something
+};
+
+be.start(main, times, 100);
